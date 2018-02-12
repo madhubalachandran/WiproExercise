ba6089d2ccc61da35cf35a692e97692e1fe8878e
madhubalachandran <madhuappdev@gmail.com>
2018-02-12 12:27:58 +0530

    URLConnection class added

---
+++ Project/URLConnection.h
@@ -0,0 +1,13 @@
+//
+//  URLConnection.h
+//  Project
+//
+//  Created by vigneshwaranm on 12/02/18.
+//  Copyright © 2018 FSS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface URLConnection : NSObject
+
+@end
---
+++ Project/URLConnection.m
@@ -0,0 +1,13 @@
+//
+//  URLConnection.m
+//  Project
+//
+//  Created by vigneshwaranm on 12/02/18.
+//  Copyright © 2018 FSS. All rights reserved.
+//
+
+#import "URLConnection.h"
+
+@implementation URLConnection
+
+@end
