378433e1822cc7b01bf5554ce01cf53361cb1e1a
Vigneshwaran Murugesan <mvigneshwaran1990@gmail.com>
2018-02-12 09:13:38 +0530

    Initial Commit

---
+++ Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by vigneshwaranm on 12/02/18.
+//  Copyright © 2018 FSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by vigneshwaranm on 12/02/18.
+//  Copyright © 2018 FSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
