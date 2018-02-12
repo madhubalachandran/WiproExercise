0c037013d32ab819b709af8be643e3633142671c
madhubalachandran <madhuappdev@gmail.com>
2018-02-12 09:13:38 +0530

    Initial Commit

---
+++ Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project
+//
+//  Created by vigneshwaranm on 12/02/18.
+//  Copyright © 2018 FSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
